9e7ce973d9d3ace944298eac80bfa0244e166aec
anastasia <anastasia.gribovskaia@gmail.com>
2023-02-05 02:40:24 +0300

    feat: implemented client-side registration form validation

--- src/core/components/registration/index.ts
+++ src/core/components/registration/index.ts
@@ -5,30 +5,38 @@ import Header from '../../components/header';
 class Registration extends Component {
     cross: HTMLElement;
     name: HTMLInputElement;
+    errorName: HTMLElement;
     email: HTMLInputElement;
+    errorEmail: HTMLElement;
     password: HTMLInputElement;
     passwordRepeat: HTMLInputElement;
+    errorPassword: HTMLElement;
     nameCheck: boolean;
     emailCheck: boolean;
     passwordCheck: boolean;
     submit: HTMLButtonElement;
+    form: HTMLFormElement;
     
     constructor(tagName: string, className: string) {
         super(tagName, className);
+        this.form = document.createElement('form');
         this.cross = document.createElement('div');
         this.name = document.createElement('input');
+        this.errorName = document.createElement('div');
         this.email = document.createElement('input');
+        this.errorEmail = document.createElement('div');
         this.password = document.createElement('input');
         this.passwordRepeat = document.createElement('input');
+        this.errorPassword = document.createElement('div');
         this.submit = document.createElement('button');
         this.nameCheck = true;
         this.emailCheck = true;
         this.passwordCheck = true;
     }
 
+    
     renderRegistration() {
-        const form = document.createElement('form');
-        form.classList.add('registration__form');
+        this.form.classList.add('registration__form');
 
         const title = document.createElement('div');
         title.textContent = 'Регистрация';
@@ -36,44 +44,46 @@ class Registration extends Component {
 
         this.cross.classList.add('registration__cross');
 
-        this.name.classList.add('registration__name', 'input');
+        this.name.classList.add('registration__name', 'input', 'input-registration');
         this.name.placeholder = 'Логин';
         this.name.type = 'text';
-        const divName = document.createElement('div');
-        divName.className = 'registration__error';
+        this.name.setAttribute('required', 'true');
+        this.name.pattern = '^[a-zA-Z][a-zA-Z0-9-_\.]*';
+        this.errorName.className = 'registration__error';
 
-        this.email.classList.add('registration__email', 'input');
+        this.email.classList.add('registration__email', 'input', 'input-registration');
         this.email.placeholder = 'E-mail адрес';
         this.email.type = 'email';
-        const divEmail = document.createElement('div');
-        divEmail.className = 'registration__error';
+        this.email.setAttribute('required', 'true');
+        this.errorEmail.className = 'registration__error';
 
-        this.password.classList.add('registration__password', 'input');
+        this.password.classList.add('registration__password', 'input', 'input-registration');
         this.password.placeholder = 'Пароль';
         this.password.type = 'password';
+        this.password.setAttribute('required', 'true');
 
-        this.passwordRepeat.classList.add('registration__password', 'input');
+        this.passwordRepeat.classList.add('registration__password', 'input', 'input-registration');
         this.passwordRepeat.placeholder = 'Повторите пароль';
         this.passwordRepeat.type = 'password';
+        this.passwordRepeat.setAttribute('required', 'true');
 
-        const divPassword = document.createElement('div');
-        divPassword.className = 'registration__error';
+        this.errorPassword.className = 'registration__error';
 
         this.submit.classList.add('button', 'registration__submit');
         this.submit.type = 'submit';
         this.submit.textContent = 'Регистрация';
 
-        this.container.appendChild(form);
-        form.appendChild(title);
-        form.appendChild(this.cross);
-        form.appendChild(this.email);
-        form.appendChild(divEmail);
-        form.appendChild(this.name);
-        form.appendChild(divName);
-        form.appendChild(this.password);
-        form.appendChild(this.passwordRepeat);
-        form.appendChild(divPassword);
-        form.appendChild(this.submit);
+        this.container.appendChild(this.form);
+        this.form.appendChild(title);
+        this.form.appendChild(this.cross);
+        this.form.appendChild(this.email);
+        this.form.appendChild(this.errorEmail);
+        this.form.appendChild(this.name);
+        this.form.appendChild(this.errorName);
+        this.form.appendChild(this.password);
+        this.form.appendChild(this.passwordRepeat);
+        this.form.appendChild(this.errorPassword);
+        this.form.appendChild(this.submit);
     }
 
     render() {
@@ -82,6 +92,82 @@ class Registration extends Component {
         this.cross.addEventListener('click', () => {
             Header.prototype.closeForm('registration');
             Header.formActive = false;
+        })
+
+        this.name.addEventListener('blur', () => {
+            this.name.classList.remove('input-valid');
+            if (this.name.value.length > 0) {
+                if (!this.name.checkValidity()) {
+                    this.nameCheck = false;
+                    this.name.classList.add('input-invalid');
+                    this.name.classList.remove('input-valid');
+                    this.errorName.textContent = `Убедитесь, что значение состоит из латинских букв, цифр, символod тире (-), подчеркивания (_) и точки (.))`;
+                } else if (this.name.value.length < 3) {    
+                    this.nameCheck = false;
+                    this.name.classList.add('input-invalid');
+                    this.name.classList.remove('input-valid');
+                    this.errorName.textContent = `Убедитесь, что это значение содержит не менее 3 символов (сейчас ${this.name.value.length}).`
+                } else if (this.name.value.length > 30) {
+                    this.nameCheck = false;
+                    this.name.classList.add('input-invalid');
+                    this.name.classList.remove('input-valid');
+                    this.errorName.textContent = `Убедитесь, что это значение содержит не более 20 символов (сейчас ${this.name.value.length}).`
+                } else {
+                    this.nameCheck = true;
+                    this.name.classList.remove('input-invalid');
+                    this.name.classList.add('input-valid');
+                    this.errorName.textContent = '';
+                }
+            }
+        })
+
+        this.email.addEventListener('blur', () => {
+            this.email.classList.remove('input-valid');
+            if (this.email.value.length > 0) {
+                if (!this.email.checkValidity()) {
+                    this.emailCheck = false;
+                    this.email.classList.add('input-invalid');
+                    this.email.classList.remove('input-valid');
+                    this.errorEmail.textContent = `Пожалуйста, введите корректный e-mail`;
+                } else {
+                    this.emailCheck = true;
+                    this.email.classList.remove('input-invalid');
+                    this.email.classList.add('input-valid');
+                    this.errorEmail.textContent = '';
+                }
+            }
+        })
+
+        this.password.addEventListener('blur', () => {
+            this.password.classList.remove('input-valid');
+            if (this.password.value.length > 0) {
+                if (this.password.value.length < 8) {
+                    this.passwordCheck = false;
+                    this.password.classList.add('input-invalid');
+                    this.password.classList.remove('input-valid');
+                    this.errorPassword.textContent = 'Недостаточно сложный пароль. Пожалуйста, введите пароль минимум 8 символов';
+                } else {
+                    this.passwordCheck = true;
+                    this.password.classList.remove('input-invalid');
+                    this.password.classList.add('input-valid');
+                    this.errorPassword.textContent = '';
+
+                    this.form.addEventListener('submit', (event) => {
+                        if (this.password.value !== this.passwordRepeat.value) {
+                            event.preventDefault();
+                            this.passwordCheck = false;
+                            this.passwordRepeat.classList.add('input-invalid');
+                            this.passwordRepeat.classList.remove('input-valid');
+                            this.errorPassword.textContent = 'Пожалуйста, повторите пароль';
+                        } else {
+                            this.passwordCheck = true;
+                            this.passwordRepeat.classList.remove('input-invalid');
+                            this.passwordRepeat.classList.add('input-valid');
+                            this.errorPassword.textContent = '';
+                        }
+                    })
+                }
+            }
         })
 
         return this.container;
